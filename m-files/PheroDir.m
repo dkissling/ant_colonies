33095411e5a5f91656c2c009db73a7f341bd5916
Lukovski <boslukas@student.ethz.ch>
2014-04-20 19:30:11 +0200

    completed PheroDir

--- m-files/PheroDir.m
+++ m-files/PheroDir.m
@@ -4,7 +4,31 @@
 %Gesammtvektor aus allen umliegenden Pheromon-Feldern
 %Wert des bisherigen Vektors: 30
 
-Vector = Vector + Feld(pos(2)-1,pos(1)-1)*[1;1]/sqrt(2); %Pheromonwirkung nach unten rechts
-Vector = Vector + Feld(pos(2),pos(1)-1)*[1;0]; %Pheromonwirkung nach rechts
-Vector = Vector + Feld(pos(2)-1,pos(1)-1)*[-1;1]/sqrt(2); %Pheromonwirkung nach oben rechts
-Vector = Vector + Feld(pos(2),pos(1)-1)*[1;0]; %Pheromonwirkung nach oben
+%Bisheriger Vektor
+vector = 30*dir;
+
+%Pheromonwirkung nach unten rechts
+vector = vector + Field_1(pos(1)-1,pos(2)-1)*[1;1]/sqrt(2); 
+%Pheromonwirkung nach rechts
+vector = vector + Field_1(pos(1),pos(2)-1)*[0;1]; 
+%Pheromonwirkung nach oben rechts
+vector = vector + Field_1(pos(1)-1,pos(2)-1)*[-1;1]/sqrt(2); 
+%Pheromonwirkung nach oben
+vector = vector + Field_1(pos(1)+1,pos(2))*[-1;0]; 
+%Pheromonwirkung nach oben links
+vector = vector + Field_1(pos(1)+1,pos(2)+1)*[-1;-1]/sqrt(2);  
+%Pheromonwirkung nach links
+vector = vector + Field_1(pos(1),pos(2)+1)*[0;-1]; 
+%Pheromonwirkung nach unten links
+vector = vector + Field_1(pos(1)-1,pos(2)+1)*[1;-1]/sqrt(2); 
+%Pheromonwirkung nach unten
+vector = vector + Field_1(pos(1)-1,pos(2))*[1;0];
+
+
+%Vektor normieren
+
+if norm(vector) ~= 0
+    dir = vector/norm(vector);
+else
+    %dir bleibt gleich
+end
